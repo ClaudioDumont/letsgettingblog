52a77b92dd38c36006b16a01b9613b1db697ae75
Claudio Dumont <claudio.dumont_ext@glintt.com>
2021-12-11 18:05:46 +0000

    changing titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lets-get-checked-blog';
+  title = 'lets-getting-blog';
 }
